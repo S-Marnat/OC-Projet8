ba01f13a83e538bf1a80cf187b4d38f3039c2331
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-07-31 16:45:46 +0200

    fix: gestion des erreurs dans les endpoints

--- Api/Controllers/TourGuideController.cs
+++ Api/Controllers/TourGuideController.cs
@@ -20,31 +20,87 @@ public class TourGuideController : ControllerBase
     [HttpGet("getLocation")]
     public async Task<ActionResult<VisitedLocation>> GetLocationAsync([FromQuery] string userName)
     {
-        var location = await _tourGuideService.GetUserLocationAsync(GetUser(userName));
-        return Ok(location);
+        var user = GetUser(userName);
+
+        if (user == null)
+        {
+            return NotFound("Le nom renseigné ne correspond à aucun utilisateur.");
+        }
+
+        try
+        {
+            var location = await _tourGuideService.GetUserLocationAsync(user);
+            return Ok(location);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Une erreur interne est survenue.");
+        }
     }
 
     [HttpGet("getNearbyAttractions")]
     public async Task<ActionResult<List<Attraction>>> GetNearbyAttractionsAsync([FromQuery] string userName)
     {
-        var visitedLocation = await _tourGuideService.GetUserLocationAsync(GetUser(userName));
-        var attractions = await _tourGuideService.GetNearByAttractionsAsync(visitedLocation);
-        var json = await _tourGuideService.CreateAttractionJsonObjectAsync(attractions, visitedLocation);
-        return Ok(json);
+        var user = GetUser(userName);
+
+        if (user == null)
+        {
+            return NotFound("Le nom renseigné ne correspond à aucun utilisateur.");
+        }
+
+        try
+        {
+            var visitedLocation = await _tourGuideService.GetUserLocationAsync(user);
+            var attractions = await _tourGuideService.GetNearByAttractionsAsync(visitedLocation);
+            var json = await _tourGuideService.CreateAttractionJsonObjectAsync(attractions, visitedLocation);
+            return Ok(json);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Une erreur interne est survenue.");
+        }
     }
 
     [HttpGet("getRewards")]
     public ActionResult<List<UserReward>> GetRewards([FromQuery] string userName)
     {
-        var rewards = _tourGuideService.GetUserRewards(GetUser(userName));
-        return Ok(rewards);
+        var user = GetUser(userName);
+
+        if (user == null)
+        {
+            return NotFound("Le nom renseigné ne correspond à aucun utilisateur.");
+        }
+
+        try
+        {
+            var rewards = _tourGuideService.GetUserRewards(user);
+            return Ok(rewards);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Une erreur interne est survenue.");
+        }
     }
 
     [HttpGet("getTripDeals")]
     public ActionResult<List<Provider>> GetTripDeals([FromQuery] string userName)
     {
-        var deals = _tourGuideService.GetTripDeals(GetUser(userName));
-        return Ok(deals);
+        var user = GetUser(userName);
+
+        if (user == null)
+        {
+            return NotFound("Le nom renseigné ne correspond à aucun utilisateur.");
+        }
+
+        try
+        {
+            var deals = _tourGuideService.GetTripDeals(user);
+            return Ok(deals);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Une erreur interne est survenue.");
+        }
     }
 
 
